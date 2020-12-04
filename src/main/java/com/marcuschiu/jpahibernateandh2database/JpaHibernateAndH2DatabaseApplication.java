105ea9fd58051940737b83a2b86049ab2015b7ec
Marcus Chiu <marcuschiu9@gmail.com>
2020-12-04 10:24:53 -0600

    initial commit

---
+++ src/main/java/com/marcuschiu/jpahibernateandh2database/JpaHibernateAndH2DatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.marcuschiu.jpahibernateandh2database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateAndH2DatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHibernateAndH2DatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcuschiu/jpahibernateandh2database/JpaHibernateAndH2DatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcuschiu.jpahibernateandh2database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateAndH2DatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
